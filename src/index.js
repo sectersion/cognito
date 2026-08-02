b9c4840cdb34bff3998b0abc85e922f5026c22ef
root <root@srv1871214.hstgr.cloud>
2026-08-02 18:00:35 +0000

    fix: remove COEP header that blocks Scramjet WASM subresource loading

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ const fastify = Fastify({
     return createServer()
       .on("request", (req, res) => {
         res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
         handler(req, res);
       })
       .on("upgrade", (req, socket, head) => {
